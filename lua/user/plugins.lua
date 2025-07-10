3c7b585bb2fc0992353be6a86079edf5dd157884
rebuilt <memoryman51@hotmail.com>
2025-07-10 12:35:17 -0700

    Configure cmp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -826,7 +826,106 @@ M.setup = function(config)
 					return col ~= 0
 						and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 				end
+				local status_cmp_ok, cmp_types = pcall(require, "cmp.types.cmp")
+				if not status_cmp_ok then
+					return
+				end
+				local ConfirmBehavior = cmp_types.ConfirmBehavior
+				local SelectBehavior = cmp_types.SelectBehavior
+				---when inside a snippet, seeks to the nearest luasnip field if possible, and checks if it is jumpable
+				---@param dir number 1 for forward, -1 for backward; defaults to 1
+				---@return boolean true if a jumpable luasnip field is found while inside a snippet
+				local function jumpable(dir)
+					local luasnip_ok, luasnip = pcall(require, "luasnip")
+					if not luasnip_ok then
+						return false
+					end
 
+					local win_get_cursor = vim.api.nvim_win_get_cursor
+					local get_current_buf = vim.api.nvim_get_current_buf
+
+					---sets the current buffer's luasnip to the one nearest the cursor
+					---@return boolean true if a node is found, false otherwise
+					local function seek_luasnip_cursor_node()
+						-- TODO(kylo252): upstream this
+						-- for outdated versions of luasnip
+						if not luasnip.session.current_nodes then
+							return false
+						end
+
+						local node = luasnip.session.current_nodes[get_current_buf()]
+						if not node then
+							return false
+						end
+
+						local snippet = node.parent.snippet
+						local exit_node = snippet.insert_nodes[0]
+
+						local pos = win_get_cursor(0)
+						pos[1] = pos[1] - 1
+
+						-- exit early if we're past the exit node
+						if exit_node then
+							local exit_pos_end = exit_node.mark:pos_end()
+							if
+								(pos[1] > exit_pos_end[1]) or (pos[1] == exit_pos_end[1] and pos[2] > exit_pos_end[2])
+							then
+								snippet:remove_from_jumplist()
+								luasnip.session.current_nodes[get_current_buf()] = nil
+
+								return false
+							end
+						end
+
+						node = snippet.inner_first:jump_into(1, true)
+						while node ~= nil and node.next ~= nil and node ~= snippet do
+							local n_next = node.next
+							local next_pos = n_next and n_next.mark:pos_begin()
+							local candidate = n_next ~= snippet and next_pos and (pos[1] < next_pos[1])
+								or (pos[1] == next_pos[1] and pos[2] < next_pos[2])
+
+							-- Past unmarked exit node, exit early
+							if n_next == nil or n_next == snippet.next then
+								snippet:remove_from_jumplist()
+								luasnip.session.current_nodes[get_current_buf()] = nil
+
+								return false
+							end
+
+							if candidate then
+								luasnip.session.current_nodes[get_current_buf()] = node
+								return true
+							end
+
+							local ok
+							ok, node = pcall(node.jump_from, node, 1, true) -- no_move until last stop
+							if not ok then
+								snippet:remove_from_jumplist()
+								luasnip.session.current_nodes[get_current_buf()] = nil
+
+								return false
+							end
+						end
+
+						-- No candidate, but have an exit node
+						if exit_node then
+							-- to jump to the exit node, seek to snippet
+							luasnip.session.current_nodes[get_current_buf()] = snippet
+							return true
+						end
+
+						-- No exit node, exit from snippet
+						snippet:remove_from_jumplist()
+						luasnip.session.current_nodes[get_current_buf()] = nil
+						return false
+					end
+
+					if dir == -1 then
+						return luasnip.in_snippet() and luasnip.jumpable(-1)
+					else
+						return luasnip.in_snippet() and seek_luasnip_cursor_node() and luasnip.jumpable(1)
+					end
+				end
 				cmp.setup({
 					snippet = {
 						expand = function(args)
@@ -840,78 +939,75 @@ M.setup = function(config)
 					--
 					-- No, but seriously. Please read `:help ins-completion`, it is really good!
 					mapping = cmp.mapping.preset.insert({
-						-- Select the [n]ext item
-						["<C-n>"] = cmp.mapping.select_next_item(),
-						-- Select the [p]revious item
-						["<C-p>"] = cmp.mapping.select_prev_item(),
-
-						-- Scroll the documentation window [b]ack / [f]orward
-						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+						["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+						["<Down>"] = cmp.mapping(
+							cmp.mapping.select_next_item({ behavior = SelectBehavior.Select }),
+							{ "i" }
+						),
+						["<Up>"] = cmp.mapping(
+							cmp.mapping.select_prev_item({ behavior = SelectBehavior.Select }),
+							{ "i" }
+						),
+						["<C-d>"] = cmp.mapping.scroll_docs(-4),
 						["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-						-- Accept ([y]es) the completion.
-						--  This will auto-import if your LSP supports it.
-						--  This will expand snippets if the LSP sent a snippet.
-						["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-						-- If you prefer more traditional completion keymaps,
-						-- you can uncomment the following lines
-						--['<CR>'] = cmp.mapping.confirm { select = true },
-						--['<Tab>'] = cmp.mapping.select_next_item(),
-						--['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-						-- Manually trigger a completion from nvim-cmp.
-						--  Generally you don't need this, because nvim-cmp will display
-						--  completions whenever it has completion options available.
-						["<C-Space>"] = cmp.mapping.complete({}),
-						["<Tab>"] = vim.schedule_wrap(function(fallback)
-							if cmp.visible() and has_words_before() then
-								cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-							else
-								fallback()
-							end
-						end),
-						["<S-Tab>"] = {
-							c = function()
+						["<C-y>"] = cmp.mapping({
+							i = cmp.mapping.confirm({ behavior = ConfirmBehavior.Replace, select = false }),
+							c = function(fallback)
 								if cmp.visible() then
-									cmp.select_prev_item()
-								else
-									cmp.complete()
-								end
-							end,
-						},
-						["<CR>"] = cmp.mapping({
-							i = function(fallback)
-								if cmp.visible() and cmp.get_active_entry() then
-									cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+									cmp.confirm({ behavior = ConfirmBehavior.Replace, select = false })
 								else
 									fallback()
 								end
 							end,
-							s = cmp.mapping.confirm({ select = true }),
-							c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
 						}),
-						-- Think of <c-l> as moving to the right of your snippet expansion.
-						--  So if you have a snippet that's like:
-						--  function $name($args)
-						--    $body
-						--  end
-						--
-						-- <c-l> will move you to the right of each of the expansion locations.
-						-- <c-h> is similar, except moving you backwards.
-						["<C-l>"] = cmp.mapping(function()
-							if luasnip.expand_or_locally_jumpable() then
+						["<Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.expand_or_locally_jumpable() then
 								luasnip.expand_or_jump()
+							elseif jumpable(1) then
+								luasnip.jump(1)
+							elseif has_words_before() then
+								-- cmp.complete()
+								fallback()
+							else
+								fallback()
 							end
 						end, { "i", "s" }),
-						["<C-h>"] = cmp.mapping(function()
-							if luasnip.locally_jumpable(-1) then
+						["<S-Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.jumpable(-1) then
 								luasnip.jump(-1)
+							else
+								fallback()
 							end
 						end, { "i", "s" }),
-
-						-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-						--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm({ select = true }),
+						-- ["<CR>"] = cmp.mapping(function(fallback)
+						-- 	if cmp.visible() then
+						-- 		local confirm_opts = { select = false }
+						-- 		local is_insert_mode = function()
+						-- 			return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
+						-- 		end
+						-- 		if is_insert_mode() then -- prevent overwriting brackets
+						-- 			confirm_opts.behavior = ConfirmBehavior.Insert
+						-- 		end
+						-- 		local entry = cmp.get_selected_entry()
+						-- 		local is_copilot = entry and entry.source.name == "copilot"
+						-- 		if is_copilot then
+						-- 			confirm_opts.behavior = ConfirmBehavior.Replace
+						-- 			confirm_opts.select = true
+						-- 		end
+						-- 		if cmp.confirm(confirm_opts) then
+						-- 			return -- success, exit early
+						-- 		end
+						-- 	end
+						-- 	fallback() -- if not exited early, always fallback
+						-- end),
 					}),
 					sources = {
 						{
