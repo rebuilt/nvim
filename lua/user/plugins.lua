d74e63ed475fb252e64f079528119edb2d3a5e8e
rebuilt <memoryman51@hotmail.com>
2025-12-02 23:14:35 -0800

    switch to using stylua as installed by nix so disable neovim installation of stylua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -717,7 +717,6 @@ M.setup = function(config)
 				-- for you, so that they are available from within Neovim.
 				local ensure_installed = vim.tbl_keys(servers or {})
 				vim.list_extend(ensure_installed, {
-					"stylua", -- Used to format Lua code
 					"solargraph",
 				})
 				require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
