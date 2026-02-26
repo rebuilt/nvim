40d4fa99760b24cf40955cbf10ee85dcf6f0231b
rebuilt <memoryman51@hotmail.com>
2026-02-26 13:08:25 -0800

    Don't add four parentheses when completing

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -4,7 +4,7 @@ M.setup = function()
 	-- vim: ts=2 sts=2 sw=2 et
 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 	local cmp = require("cmp")
-	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 	local npairs = require("nvim-autopairs")
 	npairs.add_rules(require("nvim-autopairs.rules.endwise-elixir"))
